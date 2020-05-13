2a589aafc9fa5fe0402445dbcaa20c4ac9cda903
6v6 <32081999+6v6@users.noreply.github.com>
2020-05-13 10:56:05 +0900

    Ajax로 GET, POST방식 요청

--- fintech_node/expressServer.js
+++ fintech_node/expressServer.js
@@ -6,7 +6,8 @@ app.set('views', path.join(__dirname, 'views')); // ejs file location
 app.set('view engine', 'ejs'); //select view template engine
 
 app.use(express.static(path.join(__dirname, 'public'))); // to use static asset (design)
-
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));//폼데이터를 전송하는 것을 허용
 
 
 // root 라우터
@@ -28,6 +29,23 @@ app.get('/test', function(req, res) {
 // design
 app.get('/design', function(req, res) {
     res.render('designTest');
+})
+
+//datasend Router add
+app.get('/dataSend', function(req, res){
+    res.render('dataSend');
+})
+
+app.post('/getTime', function(req, res){
+    var nowTime = new Date();
+    res.json(nowTime);
+})
+
+app.post('/getData',function(req, res){
+    console.log(req.body); //ajax로 보낸 데이터를 req.body.데이터의key 값응로 접근
+    var userData = req.body.userInputData;
+    console.log('userData = ',userData);
+    res.json(userData + "!!!!!");
 })
 
 app.listen(3000)
